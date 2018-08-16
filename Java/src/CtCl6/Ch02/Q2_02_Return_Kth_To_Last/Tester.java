e33e5379ef094d4a46e8ea13503c4fe215e431ec
Yawei Hui <ywhui30@gmail.com>
2018-08-16 17:44:57 -0400

    First commit

---
+++ Java/src/CtCl6/Ch02/Q2_02_Return_Kth_To_Last/Tester.java
@@ -0,0 +1,12 @@
+package CtCl6.Ch02.Q2_02_Return_Kth_To_Last;
+
+import CtCILibrary.CtCILibrary.AssortedMethods;
+
+public class Tester {
+
+	
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ Java/src/CtCl6/Ch02/Q2_05_Sum_Lists/PartialSum.java
@@ -0,0 +1,8 @@
+package CtCl6.Ch02.Q2_05_Sum_Lists;
+
+import CtCILibrary.CtCILibrary.LinkedListNode;
+
+public class PartialSum {
+	public LinkedListNode sum = null;
+	public int carry = 0;
+}
