8509ec4e2a38cc7acd09e46c2cbc8d79b3272068
Yawei Hui <ywhui30@gmail.com>
2018-08-17 14:58:32 -0400

    Third commit

---
+++ Java/src/CtCl6/Ch15/IntroductionSynchronizedBlocks/MyClass.java
@@ -0,0 +1,15 @@
+package CtCl6.Ch15.IntroductionSynchronizedBlocks;
+
+public class MyClass extends Thread  {
+	private String name;
+	private MyObject myObj;
+	
+	public MyClass(MyObject obj, String n) {
+		name = n;
+		myObj = obj;
+	}
+	
+	public void run() {
+		myObj.foo(name);
+	}
+}
